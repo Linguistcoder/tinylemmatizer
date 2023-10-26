6806692c4cff1f6de4a5d0f18cb895006d5e4056
NatCHS <55261832+Linguistcoder@users.noreply.github.com>
2023-10-26 18:57:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ sourcefiles = ['tinylemmatizer.c', 'cstlempy.pyx']
 
 extensions = [Extension("cstlempy", sourcefiles, extra_compile_args=["-I.", "-D_CRT_SECURE_NO_WARNINGS","-DPYTHONINTERFACE"])]
 
-setup(
+setup(name="CSTtinylemmatizer",
     ext_modules = cythonize(extensions, emit_linenums=True, compiler_directives={'language_level': 3})
 )
